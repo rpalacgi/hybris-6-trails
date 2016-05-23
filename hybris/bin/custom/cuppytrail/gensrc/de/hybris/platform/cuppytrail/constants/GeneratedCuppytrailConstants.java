76de2abffdd6e6034bcb4a2d6aabb34926d1b224
Pylyp_Bukin <pylyp.bukin@travisperkins.co.uk>
2016-05-23 17:52:05 +0300

    [hybris 6 trails] generate new extension

---
+++ hybris/bin/custom/cuppytrail/gensrc/de/hybris/platform/cuppytrail/constants/GeneratedCuppytrailConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 23, 2016 3:20:29 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package de.hybris.platform.cuppytrail.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrail";
+	
+	protected GeneratedCuppytrailConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
