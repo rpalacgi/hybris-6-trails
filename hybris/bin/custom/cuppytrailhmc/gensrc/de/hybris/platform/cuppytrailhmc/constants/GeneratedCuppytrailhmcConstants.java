2c99477c4316153f7f6dc3ca87b362becbb21c2b
Pylyp_Bukin <pylyp.bukin@travisperkins.co.uk>
2016-05-24 14:59:32 +0300

    [hybris 6 trails] Added extension for hmc

---
+++ hybris/bin/custom/cuppytrailhmc/gensrc/de/hybris/platform/cuppytrailhmc/constants/GeneratedCuppytrailhmcConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 23, 2016 3:20:29 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package de.hybris.platform.cuppytrailhmc.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailhmcConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrailhmc";
+	
+	protected GeneratedCuppytrailhmcConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
