4f537ac8a428fac08e8e0cc1c139c536a821d76e
Pylyp_Bukin <pylyp.bukin@travisperkins.co.uk>
2016-07-21 19:20:31 +0300

    [hybris 6 trails] started promotion trail implementation

---
+++ hybris/bin/custom/promotionenginetrail/src/de/hybris/promotrail/converters/populators/CustomerReviewRAOPopulator.java
@@ -0,0 +1,6 @@
+package de.hybris.promotrail.converters.populators;
+
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.customerreview.model.CustomerReviewModel;
+
+public class CustomerReviewRAOPopulator  {}
